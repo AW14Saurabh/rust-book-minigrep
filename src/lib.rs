4e22b550842ee4faec3a0e3f81f960e950497dc5
AW14Saurabh <aw14saurabh@hotmail.com>
2020-01-06 12:39:39 +0530

    calling either search based on config bool

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ use std::io::prelude::*;
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
@@ -25,7 +26,13 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let mut contents = String::new();
     f.read_to_string(&mut contents)?;
 
-    for line in search(&config.query, &contents){
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results{
         println!("{}", line);
     }
 
