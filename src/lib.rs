73f325a0d52a605191f6937b2c5ae178dbad3ef0
AW14Saurabh <aw14saurabh@hotmail.com>
2020-01-06 12:17:22 +0530

    Storing Matching Lines

--- src/lib.rs
+++ src/lib.rs
@@ -31,12 +31,14 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
     for line in contents.lines() {
         if line.contains(query) {
-            //Do something with line
+            results.push(line);
         }
     }
-    vec![]
+    results
 }
 
 #[cfg(test)]
